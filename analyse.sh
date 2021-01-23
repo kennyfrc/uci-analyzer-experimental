da5cf4955c9a65def514c5b72a474772f134f465
kennyfrc <fxkennyfrc@gmail.com>
2021-01-23 23:39:55 +0800

    improve script

--- analyse.sh
+++ analyse.sh
@@ -1,18 +1,5 @@
-while getopts e:i:o:d: option
-do
-case "${option}"
-in
-e) ENGINE=${OPTARG};;
-i) INPUT=${OPTARG};;
-o) OUTPUT=${OPTARG};;
-d) DEPTH=${OPTARG};;
-esac
-done
+#!/usr/bin/bash
+pgn-extract -Wuci 1.pgn | analyse --engine ethdev --searchdepth 8 --setoption MultiPV 8 --annotatePGN | pgn-extract -w99999 --output 1x.pgn
 
-pgn-extract -Wuci $INPUT | ./analyse --engine $ENGINE --searchdepth $DEPTH --annotatePGN | pgn-extract -w99999 --output $OUTPUT
-# pgn-extract -Wuci 1.pgn | analyse --engine rodentIV --searchdepth 8 --setoption Threads 4 -setoption PersonalityFile /home/ubuntu/engines/rodent-iv/personalities/morphy.txt --annotatePGN | pgn-extract -w99999 --output 1m.pgn
 
-# pgn-extract -Wuci 10.pgn | analyse --engine rodentIV --searchdepth 8 --setoption PersonalityFile /home/ubuntu/engines/rodent-iv/personalities/morphy.txt --annotatePGN | pgn-extract -w99999 --output 10m.pgn
-
-# no threads
-# pgn-extract -Wuci 1.pgn | analyse --engine rodent --searchdepth 8 --setoption PersonalityFile /Users/kennyfrc/documents/code/chess/rodent-iv/personalities/morphy.txt --annotatePGN | pgn-extract -w99999 --output 1m.pgn
+# screen -dmS "test2" bash -c "pgn-extract -Wuci 2.pgn | analyse --engine ethdev --searchdepth 8 --setoption MultiPV 8 --annotatePGN | pgn-extract -w99999 --output 2x.pgn"
